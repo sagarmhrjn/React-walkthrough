b564381b55a158a2e5ab33c06666b3399336c4c9
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-03 23:59:21 +0545

    outputting dynamic content

--- src/Person/person.js
+++ src/Person/person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Person = () => {
-    return <p>I'm a Person.</p>
+    return <p>I'm a Person and I'm a {Math.floor(Math.random() * 30)}</p>
 }
 
 export default Person
