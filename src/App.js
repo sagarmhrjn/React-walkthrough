40159a2ff3bcb1f376b4bce86f0c4259e9dc2d6d
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-09 20:12:37 +0545

    adding styling with stylesheets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Person from "./Person/person";
+import Person from "./Person/Person";
 import "./App.css";
 
 class App extends Component {
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Person.css';
 
 const Person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I'm a {props.age} years old.&nbsp;
         <span>{props.children}</span>
