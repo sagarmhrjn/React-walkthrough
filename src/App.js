99f94497d55d10093d765d98238f84fbde1177df
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-09 19:20:27 +0545

    using useState() hook for state manipulation for functional component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Person from "./Person/person";
 import "./App.css";
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
-      {name: "Sagar", age: 27},
-      {name: "Jason", age: 28},
-    ]
-  }
+      { name: "Sagar", age: 27 },
+      { name: "Jason", age: 28 },
+    ],
+    otherState: "some other value.",
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState("some other value.");
+
+  console.log(personsState , otherState);
+  const switchNameHandler = () => {
     // console.log("Was clicked!");
     // DO NOT DO THIS this.state.persons[0].name = "Max";
-    this.setState({
+    setPersonsState({
       persons: [
-        {name: "Max", age: 28},
-        {name: "John", age: 26}
-      ]
-    })
-  }
+        { name: "Max", age: 28 },
+        { name: "John", age: 26 },
+      ],
+    });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a react app.</h1>
-        <button onClick={this.switchNameHandler}>Switch Name!</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
-          My Hobbies: Alcohol
-        </Person>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hi, I'm a react app.</h1>
+      <button onClick={switchNameHandler}>Switch Name!</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Alcohol
+      </Person>
+    </div>
+  );
+};
 
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: "Sagar", age: 27 },
+//     { name: "Jason", age: 28 },
+//   ],
+// };
+
+// switchNameHandler = () => {
+//   // console.log("Was clicked!");
+//   // DO NOT DO THIS this.state.persons[0].name = "Max";
+//   this.setState({
+//     persons: [
+//       { name: "Max", age: 28 },
+//       { name: "John", age: 26 },
+//     ],
+//   });
+// };
