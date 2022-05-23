3e5731568de33cea894dfdb39b3740309a2d43d4
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-23 19:52:49 +0545

    list and keys

--- src/App.js
+++ src/App.js
@@ -25,14 +25,20 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Sagar", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Jason", age: 26 },
-      ],
-    });
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => p.id === id);
+
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -69,6 +75,7 @@ class App extends Component {
                 age={person.age}
                 click={() => this.deletePersonHandler(index)}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
