ce49a539b629510c3270ed958b391568275c0494
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-26 22:53:26 +0545

    pure component instead of componentshouldupdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // getDerivedStateFromProps(props, state) {
   //   console.log("[Persons.js] getDerivedStateFromProps", props);
   // }
@@ -9,14 +9,18 @@ class Persons extends Component {
   // componentWillReceiveProps(props){
   //   console.log("[Persons.js] componentWillReceiveProps", props);
   // }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("[Persons.js] shouldComponentUpdate");
-    if (nextProps.persons !== this.props.persons) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("[Persons.js] shouldComponentUpdate");
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log("[Persons.js] getSnapshotBeforeUpdate");
