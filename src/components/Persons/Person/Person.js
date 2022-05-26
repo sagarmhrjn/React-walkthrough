b229f78ef5ce9d0bbea63a2fec3cfb0718365b0e
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-26 23:28:02 +0545

    rendering adjacent jsx elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 
 import classes from "./Person.css";
-
+import Aux from "../../../hoc/Aux"
 class Person extends Component {
   render() {
     console.log("Person.js rendering...");
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        <p key="i2">{this.props.children}</p>
         <input
+          key="i3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+        ,
+      </Aux>
     );
   }
 }
