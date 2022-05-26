2500deb1d04752f1b98dbbc3d1dd17f951e156b0
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-26 23:29:43 +0545

    using React.fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,7 +6,7 @@ class Person extends Component {
   render() {
     console.log("Person.js rendering...");
     return (
-      <Aux>
+      <React.Fragment>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -18,7 +18,7 @@ class Person extends Component {
           value={this.props.name}
         />
         ,
-      </Aux>
+      </React.Fragment>
     );
   }
 }
