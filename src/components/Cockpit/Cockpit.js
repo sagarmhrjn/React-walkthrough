558d7af70bfd23b6f94205911950fa6f4515dfb1
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-26 18:29:01 +0545

    cleanup work with useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,20 +5,21 @@ const cockpit = (props) => {
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     //  Http request...
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       alert("Saved data to cloud!");
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log("[Cockpit.js] cleanup work in useEffect");
     };
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("[Cockpit.js2] useEffect");
     return () => {
       console.log("[Cockpit.js] cleanup work in 2useEffect");
     };
-  })
+  });
   let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Red;
