824d22ee94052607d8dca97e5eea4e085cb3ae8c
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-05-27 15:31:30 +0545

    passing unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-
+import classes from './Person.css';
 import Aux from "../../../hoc/Aux";
+import withClass from "../../../hoc/withClass";
+
 class Person extends Component {
   render() {
     console.log("Person.js rendering...");
@@ -22,4 +24,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
