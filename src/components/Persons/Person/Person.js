97ea33228e3e3089123f1f646a41e98c78d409dd
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-06-10 00:07:45 +0545

    using ref and fixed proptypes typo

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,15 @@ import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("Person.js rendering...");
     return (
@@ -16,6 +25,8 @@ class Person extends Component {
         <input
           key="i3"
           type="text"
+          // ref={(inputEl) => (this.inputElement = inputEl)}
+          ref={this.inputElementRef}
           onChange={this.props.changed}
           value={this.props.name}
         />
@@ -25,7 +36,7 @@ class Person extends Component {
   }
 }
 
-Person.PropTypes = {
+Person.propTypes = {
   click: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
