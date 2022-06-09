d3aff5f30ab8fb67bd96f94375b4e3d3afad8c6b
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-06-10 00:15:32 +0545

    refs with react hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     //  Http request...
-    const timer = setTimeout(() => {
-      alert("Saved data to cloud!");
-    }, 1000);
+    // const timer = setTimeout(() => {
+    //   alert("Saved data to cloud!");
+    // }, 1000);
+    toggleBtnRef.current.click();
     return () => {
       clearTimeout(timer);
       console.log("[Cockpit.js] cleanup work in useEffect");
@@ -36,7 +39,11 @@ const cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>Hi, I'm a React App</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button
+        ref={toggleBtnRef}
+        className={btnClass}
+        onClick={props.clicked}
+      >
         Toggle Persons
       </button>
     </div>
